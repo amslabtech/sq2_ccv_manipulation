a69c910bca81193bbb6163d94baa4a3a9a7c8893
RIckyBan <ricky1231@g.ecc.u-tokyo.ac.jp>
2019-09-20 14:28:48 +0900

    add subplots

--- collision_detection.py
+++ collision_detection.py
@@ -1,18 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
- 
+from sensor_msgs.msg import LaserScan
+
 class CollisionDetector(Node):
 
     def __init__(self):
         super().__init__("collision_detection")
-        self.sub_scan1 = self.create_subscription(String, '/scan1', self.scan_sub, 10)
-        self.sub_scan2 = self.create_subscription(String, '/scan2', self.scan_sub, 10)
-        self.sub_scan3 = self.create_subscription(String, '/scan3', self.scan_sub, 10)
+        self.sub_scan1 = self.create_subscription(LaserScan, '/scan1', self.scan_sub, 10)
+        self.sub_scan2 = self.create_subscription(LaserScan, '/scan2', self.scan_sub, 10)
+        self.sub_scan3 = self.create_subscription(LaserScan, '/scan3', self.scan_sub, 10)
+
+        self.fig = plt.figure(figsize=(15,5))
+        self.axes = []
+        for i in range(3):
+            self.axes.append(self.fig.add_subplot(1, 3, i+1))
 
     def scan_sub(self, oscan):
-        print(oscan.data)
+        print(oscan.header)
+        print(oscan.ranges)
+        label = oscan.header.frame_id
+        scan_id = int(label[5]) - 1
+        self.axes[scan_id].set_title(label)
+        self.axes[scan_id].cla()
+        self.axes[scan_id].plot(np.array(oscan.ranges))
+        plt.pause(0.1)
 
     def show_graph(self):
         pass
